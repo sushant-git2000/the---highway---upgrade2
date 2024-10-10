27654797c97f1e1ff979b5769bd8348cd2e6ce08
sushant-git2000 <stkolekar2000@gmail.com>
2024-10-10 20:26:51 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("Welcome to The Highway!");
+// src/App.js
+import React from 'react';
+import 'style.css';
+import Marquee from './Marquee';
+
+const App = () => {
+    return (
+        <div>
+            <header>
+                <h1>Welcome to The Highway</h1>
+                {/* Navigation goes here */}
+            </header>
+
+            <Marquee />
+
+            <section id="latest-news">
+                <h2>Latest News</h2>
+                {/* Latest news articles go here */}
+            </section>
+
+            {/* Other sections */}
+        </div>
+    );
+};
+
+export default App;
